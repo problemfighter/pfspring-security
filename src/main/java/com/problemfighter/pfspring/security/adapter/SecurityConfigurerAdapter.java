532a6cd8486f8d92c87c724f0771feab6dc9c192
hmtmcse <hmtm.cse@gmail.com>
2020-12-11 23:42:09 +0600

    Auth

--- src/main/java/com/problemfighter/pfspring/security/adapter/SecurityConfigurerAdapter.java
+++
@@ -1,6 +0,0 @@
-package com.problemfighter.pfspring.security.adapter;
-
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-public class SecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-}
---
+++ src/main/java/com/problemfighter/pfspring/security/config/SecurityConfigurer.java
@@ -0,0 +1,13 @@
+package com.problemfighter.pfspring.security.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+
+}
