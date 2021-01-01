55e0389a3f77f10f3488130c741df485868574dc
hmtmcse <hmtm.cse@gmail.com>
2021-01-01 19:41:30 +0600

    added basic logic

--- src/main/java/com/problemfighter/pfspring/security/config/SecurityConfigurer.java
+++ src/main/java/com/problemfighter/pfspring/security/config/SecurityConfigurer.java
@@ -2,6 +2,7 @@ package com.problemfighter.pfspring.security.config;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -10,4 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
 }
--- src/main/java/com/problemfighter/pfspring/security/service/OperatorDetailsService.java
+++ src/main/java/com/problemfighter/pfspring/security/service/OperatorDetailsService.java
@@ -1,16 +1,23 @@
 package com.problemfighter.pfspring.security.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OperatorDetailsService implements UserDetailsService {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User.UserBuilder touhid = User.builder().username("touhid").password(passwordEncoder.encode("touhid")).roles("USER");
+        return touhid.build();
     }
 
 }
